54a1d6c86b64e68819dd41ec32ae09e0b03d06d2
ejioguvictor <decagon@MacBook-Air-6.local>
2022-05-08 04:02:56 +0100

    feat: fetched data rendered on the UI

---
+++ src/columns.js
@@ -0,0 +1,33 @@
+export const COLUMNS = [
+    {
+      Header: 'Id',
+      accessor: 'id',
+      disableFilters: true,
+      sticky: 'left'
+    },
+    {
+      Header: 'Planet',
+      accessor: 'planet',
+      sticky: 'left'
+    },
+    {
+      Header: 'Climate',
+      accessor: 'climate',
+      sticky: 'left'
+    },
+    {
+      Header: 'Terrain',
+      accessor: 'terrain',
+      sticky: 'left'
+    },
+    {
+      Header: 'Population',
+      accessor: 'population',
+      sticky: 'left'
+    },
+    {
+      Header: 'Diameter',
+      accessor: 'diameter',
+      sticky: 'left'
+    },
+  ]
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,24 +1,66 @@
 import react, {useState, useEffect} from 'react'
 import axios from 'axios'
+import MOCK_DATA from  "../../MOCK_DATA.json"
+import {useTable} from 'react-table'
+import { COLUMNS } from '../../columns'
 
 import React from 'react'
 
 export default function Table() {
+    const columns = COLUMNS
     const [page, setPage] = useState(1)
-    const [tableData, setTableData] = useState([])
+    const [data, setTableData] = useState(MOCK_DATA)
 
     const getData = async() => {
-       let result = await axios.get(`https://swapi.dev/api/planets/?page=${page}`)
-       let fetchedData = result.data.results
-       console.log(fetchedData)
-       setTableData(fetchedData)
+        try {
+        let result = await axios.get(`https://swapi.dev/api/planets/?page=${page}`)
+        let fetchedData = result.data.results
+        setTableData(fetchedData)
+        } catch (error) {
+           console.error(error) 
+        }
+       
     }
 
     useEffect(()=> {
         getData()
     },[])
 
+    const {getTableProps,getTableBodyProps,headerGroups,footerGroups,rows,prepareRow} = useTable({columns,data})
+
   return (
-    <div>Table</div>
+    <div>
+        <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}> {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map(footerGroup => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map(column => (
+                <td {...column.getFooterProps()}>{column.render('Footer')}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </div>
   )
 }
